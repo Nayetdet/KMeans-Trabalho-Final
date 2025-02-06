f75fa452bc3f615a13f93143a391fb5dad3325dc
Isaac Duarte <isaacunhaid@gmail.com>
2025-02-06 16:42:51 -0300

    Feat - Exportação dos dados do Histograma em .txt

--- tests/src/histogram.c
+++ tests/src/histogram.c
@@ -8,3 +8,28 @@ void computeHistogram(const unsigned char *const data, unsigned char histogram[H
         histogram[data[i]]++;
     }
 }
+
+void salvarHistograma(const char *filename, unsigned char histogram[HISTOGRAM_SIZE]){
+
+    char filepath[256];
+    snprintf(filepath, sizeof(filepath), "%s/%s", OUTPUT_PATH, filename);
+
+
+    FILE *file = fopen(filename, "w");
+    if (!file)
+    {
+        perror("ERRO: Erro ao abrir o arquivo para escrita");
+        return;
+    }
+
+    fprintf(file, "[ ");
+    for (unsigned i = 0; i < HISTOGRAM_SIZE; i++)
+    {
+        fprintf(file, " %u, ", histogram[i]);      
+    }
+    fprintf(file, " ]");
+    
+
+    fclose(file);
+    printf("Histograma salvo em: %s\n", filepath);
+}
