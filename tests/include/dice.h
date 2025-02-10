e7c5af628dc5d28f0d2fdc959c0973d133aea9d3
Nayetdet <jpmoreiracampos21@gmail.com>
2025-02-10 10:48:50 -0300

    docs: adicionado o cabeçalho nos arquivos do projeto

--- tests/include/dice.h
+++ tests/include/dice.h
@@ -1,3 +1,29 @@
+// ∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗
+// * Aluno: João Pedro Moreira Campos de Lima              *
+// * Matrícula: 20241045050391                             *
+// * Avaliação 04: Trabalho Final                          *
+// * 04.505.23 − 2024.2 − Prof. Daniel Ferreira            *
+// * Compilador: gcc (MinGW.org GCC-6.3.0-1) 6.3.0         *
+// ∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗
+// * Aluno: Isaac da Cunha Cavalcanti Duarte               *
+// * Matrícula: 20241045050375                             *
+// * Avaliação 04: Trabalho Final                          *
+// * 04.505.23 − 2024.2 − Prof. Daniel Ferreira            *
+// * Compilador: gcc (MinGW.org GCC-6.3.0-1) 6.3.0         *
+// ∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗
+// * Aluno: Josué Rodrigues Avelino                        *
+// * Matrícula: 20241045050219                             *
+// * Avaliação 04: Trabalho Final                          *
+// * 04.505.23 − 2024.2 − Prof. Daniel Ferreira            *
+// * Compilador: gcc (MinGW.org GCC-6.3.0-1) 6.3.0         *
+// ∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗
+// * Aluno: Orleoncio Maciel                               *
+// * Matrícula: 20181045050165                             *
+// * Avaliação 04: Trabalho Final                          *
+// * 04.505.23 − 2024.2 − Prof. Daniel Ferreira            *
+// * Compilador: gcc (MinGW.org GCC-6.3.0-1) 6.3.0         *
+// ∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗
+
 #ifndef DICE_H
 #define DICE_H
 
